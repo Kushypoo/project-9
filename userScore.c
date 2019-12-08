573a8d73fa96f14dba39d06274084f1555475ee5
Kushypoo <kushtrim_m2000@icloud.com>
2019-12-08 03:18:06 -0800

    Add files via upload

---
+++ userScore.c
@@ -0,0 +1,13 @@
+_Bool checkMatch(*/these ints are the board dimension, not user input*/ int boardDimension, char board[][boardDimension], int r1, int r2, int c1, int c2){
+	_Bool match = false;
+	if(board[r1-1][c1-1] == board[r2-1][c2-1]){
+		match = true;
+		return match;
+	}else{
+		return match;
+	}
+}
+
+int userScore(int userDifficulty){
+	
+}
